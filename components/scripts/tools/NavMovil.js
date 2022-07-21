af91c70918bc7e922805c1cd3ed41f0ef0360161
Photenix <juanmanuelpinor@gmail.com>
2022-07-21 14:54:44 -0500

    web

--- components/scripts/tools/NavMovil.js
+++ components/scripts/tools/NavMovil.js
@@ -12,25 +12,43 @@ const lightNavMovil = () => {
 function selectNavMovil () {
     const navMovil = document.getElementById("nav-movil")
 
-    let navOption = {
-        home : "../..",
-        message : "/components/pages/message.html",
-        notification : "/components/pages/communities.html",
-        profile : "/components/pages/profile.html",
-    }
 
     const location = document.location.pathname
 
-    if( location == "/"){
-        navOption["home"] = "."
+    let re = /\/\w+\/\w+\//
+    let path;
+    try{
+        path = location.match(re)[0]
+    }catch{
+        path = "none" 
+    }
+    
+
+    let navOption = {
+        home : "../..",
+        message : path + "message.html",
+        notification : path + "communities.html",
+        profile : path + "profile.html",
+    }
+
+    if( location == "/" || location == "/Pet_Work/" ){
+        navOption = {
+            home : location == "/Pet_Work/" ?"/Pet_Work/" :".",
+            message : "components/pages/message.html",
+            notification : "components/pages/communities.html",
+            profile : "components/pages/profile.html",
+        }
     }
 
     for (const key in navOption) {
         const selectionNav = navMovil.querySelector(`#nav-${key}`)
         selectionNav.addEventListener("click",() =>{
-            document.location.pathname = navOption[key]
+            if( path == "none") document.location.pathname += navOption[key]
+            else { document.location.pathname = navOption[key] }
         })
     }
+
+    
 }
 
 export { lightNavMovil, selectNavMovil }
